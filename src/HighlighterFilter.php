5802d6fd81491a532654eb1cd96a48bbffb28c3b
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-10-14 00:11:54 +0300

    feat: add highlighter filter

---
+++ src/HighlighterFilter.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Superrosko\PhpTextDecorators;
+
+use Exception;
+use Highlight\Highlighter;
+
+final class HighlighterFilter extends TextDecorator
+{
+    /**
+     * @var Highlighter
+     */
+    private Highlighter $highlighter;
+
+    public function __construct(TextDecoratorInterface $textDecorator)
+    {
+        $this->highlighter = new Highlighter();
+
+        parent::__construct($textDecorator);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format(string $text): string
+    {
+        $text = parent::format($text);
+
+        return $this->highlightContent($text);
+    }
+
+    /**
+     * @param  string  $text
+     * @return string
+     */
+    public function highlightContent(string $text): string
+    {
+        $result = preg_replace_callback(
+            '~<pre>\s*<code(?:.*?)class="language-(?<language>.*?)"(?:.*?)>\s*(?<code>.*?)\s*</code>\s*</pre>~iu',
+            fn (array $matches) => $this->prepareCode(
+                (string) $matches['language'],
+                (string) $matches['code'],
+            ),
+            $text
+        );
+
+        return $result ?? $text;
+    }
+
+    /**
+     * @param  string  $language
+     * @param  string  $code
+     * @return string
+     */
+    public function prepareCode(string $language, string $code): string
+    {
+        $result = '';
+        try {
+            $highlighted = $this->highlighter->highlight($language, $code);
+            $result .= '<pre><code class="hljs '.(string) $highlighted->language.'">';
+            $result .= (string) $highlighted->value;
+            $result .= '</code></pre>';
+        } catch (Exception $e) {
+            $result .= '<pre><code class="hljs">';
+            $result .= htmlentities($code);
+            $result .= '</code></pre>';
+        }
+
+        return $result;
+    }
+}
---
+++ tests/unit/HighlighterFilterTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unit;
+
+use Codeception\Test\Unit;
+use Exception;
+use Faker\Factory;
+use Faker\Generator;
+use Highlight\Highlighter;
+use Superrosko\PhpTextDecorators\EmptyFilter;
+use Superrosko\PhpTextDecorators\HighlighterFilter;
+use UnitTester;
+
+class HighlighterFilterTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected UnitTester $tester;
+
+    /**
+     * @var HighlighterFilter
+     */
+    public HighlighterFilter $filter;
+
+    /**
+     * @var Generator
+     */
+    public Generator $fakerFactory;
+
+    /**
+     * @var Highlighter
+     */
+    private Highlighter $highlighter;
+
+    /**
+     * @return void
+     */
+    protected function _before()
+    {
+        $filter = new EmptyFilter();
+        $this->filter = new HighlighterFilter($filter);
+        $this->fakerFactory = Factory::create();
+        $this->highlighter = new Highlighter();
+    }
+
+    public function testText(): void
+    {
+        $text = $this->fakerFactory->text;
+        $this->assertEquals($text, $this->filter->format($text));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testTextWithCode(): void
+    {
+        $code = '<?php phpinfo(); ?>';
+        $textCode = '<pre><code class="language-php">'.$code.'</code></pre>';
+        $textCodeHighlighted = (string) $this->highlighter->highlight('php', $code)->value;
+
+        $this->assertStringContainsString($textCodeHighlighted, $this->filter->format($textCode));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testHighlightContent(): void
+    {
+        $code = '<?php phpinfo(); ?>';
+        $textCode = '<pre><code class="language-php">'.$code.'</code></pre>';
+        $textCodeHighlighted = (string) $this->highlighter->highlight('php', $code)->value;
+        $this->assertStringContainsString($textCodeHighlighted, $this->filter->highlightContent($textCode));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testPrepareCode(): void
+    {
+        $code = '<?php phpinfo(); ?>';
+        $textCodeHighlighted = (string) $this->highlighter->highlight('php', $code)->value;
+        $this->assertStringContainsString($textCodeHighlighted, $this->filter->prepareCode('php', $code));
+        $this->assertEquals(
+            '<pre><code class="hljs">&lt;?php phpinfo(); ?&gt;</code></pre>',
+            $this->filter->prepareCode('test-exception', $code)
+        );
+    }
+}
